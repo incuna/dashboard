591b72ecd6d0790e1df4fd7a24d90990dcc0f80a
George Hickman <george@ghickman.co.uk>
2011-04-27 21:05:03 +0100

    passed request to the view so that static files could be loaded

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(request, extra_context = None):
-    return render_to_response('index.html')
+    context = RequestContext(request)
+    if extra_context != None:
+        context.update(extra_context)
+
+    return render_to_response('index.html', context)
+
