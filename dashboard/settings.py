b5e12ad3e75071c44ea55c9d386674d0b3b67feb
George Hickman <george@ghickman.co.uk>
2011-04-27 20:54:31 +0100

    fixed static media dir and css location in index page

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -59,8 +59,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = join(DIRNAME, 'static')
-print STATIC_ROOT
+STATIC_ROOT = join(DIRNAME, 'static_media')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -76,7 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #('', join(DIRNAME, 'static')),
+    ('', join(DIRNAME, 'static')),
 )
 
 # List of finder classes that know how to find static files in
