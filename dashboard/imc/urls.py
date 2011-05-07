69a0af8d2e0ad6e6543a28db5fe7058a445abdb8
George Hickman <george@ghickman.co.uk>
2011-05-07 21:24:54 +0100

    Cleaned up the templates and refactored the movie view to current.
    Renamed the movie view to current and created a movie view to take the slug var
    
    Moved templates around to suit this and tidied up the styles after removing some non-semantic elements

--- dashboard/imc/urls.py
+++ dashboard/imc/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('imc.views',
-    url(r'^$', 'index', name='index'),
-    url(r'^movie/', 'movie', name='movie'),
-    url(r'^dvd-request/(.*)/', 'dvd_request', name='dvd-request'),
+    url(r'^$', 'movie', name='current'),
+    url(r'^(?P<slug>.*)/$', 'movie', name='movie'),
+    #url(r'^dvd-request/(.*)/', 'dvd_request', name='dvd-request'),
+    url(r'^widget/$', 'widget', name='widget'),
 )
 
--- dashboard/imc/views.py
+++ dashboard/imc/views.py
@@ -1,25 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.shortcuts import redirect, render_to_response
+from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
 
-from forms import DvdRequestForm, MovieRatingForm
+from forms import MovieRatingForm
 from models import Movie, Rating
 
-def index(request, extra_context = None):
-    context = RequestContext(request)
-    if extra_context != None:
-        context.update(extra_context)
-    context.update({'movie': Movie.objects.current_movie()})
-    return render_to_response('imc/index.html', context)
-
 @login_required
-def movie(request, extra_context = None):
+def current(request, extra_context=None):
     context = RequestContext(request)
     if extra_context != None:
         context.update(extra_context)
 
-    movie = Movie.objects.current_movie()
+    movie = Movie.objects.current()
     if request.method == 'POST':
         form = MovieRatingForm(request.POST)
         if form.is_valid():
@@ -34,25 +27,47 @@ def movie(request, extra_context = None):
             context.update({'rating': str(round(Rating.objects.get_rating(movie=movie)['rating'], 0))[:-2]})
 
     context.update({'form': form, 'movie': movie, 'user': request.user})
-    return render_to_response('imc/movie.html', context)
+    return render_to_response('imc/current.html', context)
 
 @login_required
-def dvd_request(request, movie, extra_context = None,):
+def movie(request, slug, extra_context=None):
     context = RequestContext(request)
     if extra_context != None:
         context.update(extra_context)
 
-    if request.method == 'POST':
-        form = DvdRequestForm(request.POST)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.user = request.user
-            instance.movie = movie
-            return redirect(reverse('movie'))
-    else:
-        form = DvdRequestForm()
+    movie = get_object_or_404(Movie, slug=slug)
+    if Rating.objects.filter(user=request.user, movie=movie):
+        context.update({
+            'form': MovieRatingForm,
+            'rating': str(round(Rating.objects.get_rating(movie=movie)['rating'], 0))[:-2]
+        })
 
-    context.update({'form': form})
-    return redirect(reverse('movie'))
+    context.update({'movie': movie})
+    return render_to_response('imc/movie.html', context)
 
+def widget(request, extra_context = None):
+    context = RequestContext(request)
+    if extra_context != None:
+        context.update(extra_context)
+    context.update({'movie': Movie.objects.current()})
+    return render_to_response('imc/index.html', context)
+
+#@login_required
+#def dvd_request(request, movie, extra_context = None,):
+    #context = RequestContext(request)
+    #if extra_context != None:
+        #context.update(extra_context)
+
+    #if request.method == 'POST':
+        #form = DvdRequestForm(request.POST)
+        #if form.is_valid():
+            #instance = form.save(commit=False)
+            #instance.user = request.user
+            #instance.movie = movie
+            #return redirect(reverse('movie'))
+    #else:
+        #form = DvdRequestForm()
+
+    #context.update({'form': form})
+    #return redirect(reverse('movie'))
 
