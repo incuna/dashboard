1a60ec19a98382b3c06bf70d13778d24745b335e
George Hickman <george@ghickman.co.uk>
2011-05-10 13:58:32 +0100

    pointed the base movie club link to the current film

--- dashboard/imc/urls.py
+++ dashboard/imc/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('imc.views',
-    url(r'^$', 'movie', name='current'),
+    url(r'^$', 'current', name='current'),
     url(r'^(?P<slug>.*)/$', 'movie', name='movie'),
     url(r'^dvd-request/(.*)/', 'dvd_request', name='dvd-request'),
     url(r'^widget/$', 'widget', name='widget'),
