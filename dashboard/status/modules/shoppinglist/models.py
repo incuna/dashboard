250a4aa5d0f7237a22bc4f621628e127e1816695
George Hickman <george@ghickman.co.uk>
2011-05-12 22:10:21 +0100

    Link shoppinglist items to a user

--- dashboard/status/modules/shoppinglist/models.py
+++ dashboard/status/modules/shoppinglist/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from profiles.models import Profile
+
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(null=True, blank=True, max_length=255)
+    description = models.CharField(max_length=255, null=True, blank=True)
     bought = models.BooleanField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    added_by = models.ForeignKey(Profile)
 
     def __unicode__(self):
         return self.name
