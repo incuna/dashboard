eb3eb3e56d49e3da5dc7c9cd94418284ff81ec9e
George Hickman <george@ghickman.co.uk>
2011-05-12 22:10:34 +0100

    Required a login to view the main dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+@login_required
 def index(request, extra_context = None):
     context = RequestContext(request)
     if extra_context != None:
