b9c2e99f2af8451211d4912b3299c2a9c0dfecd0
George Hickman <george@ghickman.co.uk>
2011-05-27 10:51:13 +0100

    moved the media url

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -61,7 +61,7 @@ MEDIA_ROOT = join(DIRNAME, 'client_media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/client_media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
