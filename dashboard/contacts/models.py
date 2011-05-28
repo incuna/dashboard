18b4d5a29e393944eed9f0d029f8a4ed1444ac6a
George Hickman <george@ghickman.co.uk>
2011-05-28 11:00:34 +0100

    Linked contacts to profiles instead of the django user object

--- dashboard/contacts/models.py
+++ dashboard/contacts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+from profiles.models import Profile
 
 class Contact(models.Model):
     name = models.CharField(max_length=255, null=True, blank=True, help_text='Optional: If a User is picked and no name given, one will be generated.')
-    user = models.OneToOneField(User, null=True)
+    user = models.OneToOneField(Profile, null=True)
     extension = models.CharField(max_length=7)
 
     class Meta:
