4d3d928a3d8ce0042ee14d20ccaba249a3aa5f50
George Hickman <george@ghickman.co.uk>
2011-06-25 13:08:50 +0100

    Added new movie urls

--- dashboard/imc/urls.py
+++ dashboard/imc/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('imc.views',
-    url(r'^$', 'current', name='current-movie'),
-    url(r'^widget/$', 'widget', name='widget'),
+    url(r'^$', 'index', name='movie-index'),
+    url(r'^current/$', 'current', name='movie-current'),
     url(r'^group/$', 'group', name='movie-group-rating'),
+    url(r'^submit/$', 'current', name='movie-submit'),
+    url(r'^widget/$', 'widget', name='movie-widget'),
     url(r'^(?P<slug>.*)/$', 'movie', name='movie'),
 )
 
