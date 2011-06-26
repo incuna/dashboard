e5b39be0c3d3c4b5b7df9a1530c5d9a884d5207e
George Hickman <george@ghickman.co.uk>
2011-06-26 13:43:32 +0100

    fix imc urls in various places

--- dashboard/imc/urls.py
+++ dashboard/imc/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('imc.views',
     url(r'^$', 'index', name='movie-index'),
     url(r'^current/$', 'current', name='movie-current'),
-    url(r'^group/$', 'group', name='movie-group-rating'),
+    url(r'^group/$', 'group_rating', name='movie-group-rating'),
     url(r'^submit/$', 'current', name='movie-submit'),
     url(r'^widget/$', 'widget', name='movie-widget'),
     url(r'^(?P<slug>.*)/$', 'movie', name='movie'),
