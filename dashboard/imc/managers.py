402699160c9d60dd98d18f8151338de17e5dcde2
George Hickman <george@ghickman.co.uk>
2011-08-05 07:35:45 +0100

    wrap the code that looks up the latest period of time so it doesn't kill the whole project

--- dashboard/imc/managers.py
+++ dashboard/imc/managers.py
@@ -1,9 +1,17 @@
+from django.core.mail import mail_admins
 from django.db.models import Avg, Manager
+from django.http import HttpResponseRedirect
 
 class PeriodManager(Manager):
     def last_finish(self):
         """Returns the latest Period by finish date"""
-        return self.get_query_set().all().order_by('-finish')[0]
+        try:
+            self.get_query_set().all().order_by('-finish')[0]
+        except IndexError:
+            mail_admins('No IMC Periods of Time',
+                    'The IMC app needs you to create some Periods of Time! (imc/managers.py L11)')
+            return HttpResponseRedirect('/')
+
 
 class RatingManager(Manager):
     def get_rating(self, movie):
