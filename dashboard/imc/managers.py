96d33368e2286d62e0d652bcfb4cc065e6433e01
George Hickman <george@ghickman.co.uk>
2011-08-06 11:23:08 +0100

    remove the period manager from the managers file

--- dashboard/imc/managers.py
+++ dashboard/imc/managers.py
@@ -1,17 +1,4 @@
-from django.core.mail import mail_admins
 from django.db.models import Avg, Manager
-
-class PeriodManager(Manager):
-    def last_finish(self):
-        """Returns the latest Period by finish date"""
-        periods = self.get_query_set().all().order_by('-finish')
-        if periods:
-            return periods[0]
-        else:
-            mail_admins('No IMC Periods of Time',
-                    'The IMC app needs some attention, I\'ve created a blank one for'
-                    ' now until the default time away (imc/managers.py L11)')
-            return periods
 
 class RatingManager(Manager):
     def get_rating(self, movie):
