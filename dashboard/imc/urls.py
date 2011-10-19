04ef24d70d6d622f3929108f97e455511c73a990
George Hickman <george@ghickman.co.uk>
2011-10-19 14:18:28 +0100

    Refactor previous movies view to use class based generic views

--- dashboard/imc/urls.py
+++ dashboard/imc/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, url
 
+from imc.views import PreviousMovieListView
+
 urlpatterns = patterns('imc.views',
     url(r'^$', 'index', name='movie-index'),
     url(r'^current/$', 'current', name='movie-current'),
     url(r'^group/$', 'group_rating', name='movie-group-rating'),
-    url(r'^previous/$', 'previous', name='movie-previous'),
+    url(r'^previous/$', PreviousMovieListView.as_view(), name='movie-previous'),
     url(r'^submit/$', 'submit', name='movie-submit'),
     url(r'^widget/$', 'widget', name='movie-widget'),
     url(r'^(?P<slug>.*)/$', 'movie', name='movie'),
--- dashboard/imc/views.py
+++ dashboard/imc/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -5,6 +7,7 @@ from django.forms.models import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
+from django.views.generic import ListView
 
 from forms import MovieRatingForm, MovieRatingInlineForm, MovieSubmissionForm
 from models import Movie, Rating
@@ -71,13 +74,11 @@ def movie(request, slug, extra_context=None, template_name='imc/movie.html'):
     context.update({'movie': movie, 'rating': Rating.objects.get_rating(movie=movie)})
     return render_to_response(template_name, context)
 
-@login_required
-def previous(request, extra_context=None, template_name='imc/previous.html'):
-    context = RequestContext(request)
-    if extra_context != None:
-        context.update(extra_context)
-    context.update({'movies': Movie.objects.previous()})
-    return render_to_response(template_name, context)
+class PreviousMovieListView(ListView):
+    template_name = 'imc/previous.html'
+
+    def get_queryset(self):
+        return Movie.objects.filter(period__finish__lt=datetime.now())
 
 @login_required
 def submit(request, extra_context=None, template_name='imc/submit.html'):
