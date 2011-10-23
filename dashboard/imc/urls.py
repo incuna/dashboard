b6e37d57254f7d9108fae05f0ac2960a68012736
George Hickman <george@ghickman.co.uk>
2011-10-23 09:21:39 +0100

    Give the pool view a url

--- dashboard/imc/urls.py
+++ dashboard/imc/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls.defaults import patterns, url
 from django.views.generic import DetailView
 
 from imc.models import Movie
-from imc.views import Current, Index, Previous, Submit, Widget
+from imc.views import Current, Index, Pool, Previous, Submit, Widget
 
 urlpatterns = patterns('imc.views',
     url(r'^$', Index.as_view(), name='movie-index'),
     url(r'^current/$', Current.as_view(), name='movie-current'),
     url(r'^group/$', 'group_rating', name='movie-group-rating'),
+    url(r'^pool/$', Pool.as_view(), name='movie-pool'),
     url(r'^previous/$', Previous.as_view(), name='movie-previous'),
     url(r'^submit/$', Submit.as_view(), name='movie-submit'),
     url(r'^widget/$', Widget.as_view(), name='movie-widget'),
