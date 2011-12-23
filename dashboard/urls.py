559b5399322dae902ba4851546d2c58a11538ddf
Marc Tamlyn <marc.tamlyn@gmail.com>
2011-12-23 15:46:10 +0000

    Use simple TemplateView for the home page.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls.defaults import include, patterns, url
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 admin.site.unregister(Site)
 admin.site.unregister(User)
 
 urlpatterns = patterns('',
-    url(r'^$', 'views.index', name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^movie-club/', include('imc.urls')),
     url(r'^shopping-list/', include('status.modules.shoppinglist.urls')),
