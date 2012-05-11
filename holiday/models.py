b10028d5d9131057a117b099d566647f6b2d1eff
Charlie Denton <charlie@meshy.co.uk>
2012-05-11 15:05:07 +0100

    Tidy the code a little

--- holiday/models.py
+++ holiday/models.py
@@ -1,7 +1,6 @@
 import datetime
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
@@ -13,9 +12,9 @@ from profiles.models import Profile
 
 DATE_FORMAT = '%Y-%m-%d'
 
-if not hasattr(settings,'HOLIDAY_YEAR_START_DAY'):
+if not hasattr(settings, 'HOLIDAY_YEAR_START_DAY'):
     settings.HOLIDAY_YEAR_START_DAY = 1
-if not hasattr(settings,'HOLIDAY_YEAR_START_MONTH'):
+if not hasattr(settings, 'HOLIDAY_YEAR_START_MONTH'):
     settings.HOLIDAY_YEAR_START_MONTH = 1
 
 
@@ -23,53 +22,10 @@ def get_year_start_end(yearoffset=0):
     now = datetime.date.today()
     yearstart = datetime.date(day=settings.HOLIDAY_YEAR_START_DAY, month=settings.HOLIDAY_YEAR_START_MONTH, year=now.year)
     if yearstart > now:
-        yearstart = datetime.date(day=yearstart.day, month=yearstart.month, year=yearstart.year-1)
-    yearstart = datetime.date(day=yearstart.day, month=yearstart.month, year=yearstart.year+yearoffset)
-    yearend = datetime.date(day=yearstart.day, month=yearstart.month, year=yearstart.year+1)
+        yearstart = datetime.date(day=yearstart.day, month=yearstart.month, year=yearstart.year - 1)
+    yearstart = datetime.date(day=yearstart.day, month=yearstart.month, year=yearstart.year + yearoffset)
+    yearend = datetime.date(day=yearstart.day, month=yearstart.month, year=yearstart.year + 1)
     return yearstart, yearend
-
-
-# class Employee(User):
-#     """An Employee. Extends User by adding details of employment required for calculating holiday."""
-#     holiday_per_annum = models.IntegerField(_('annual holiday'), default=settings.HOLIDAY_DEFAULT_ANNUAL_DAYS)
-#     start_date = models.DateField(_('start date'))
-#     end_date = models.DateField(_('end date'), blank=True, null=True)
-#     manager = models.ForeignKey('self', null=True, blank=False, related_name='managed_employees')
-#     is_manager = models.BooleanField(_('is manager'), default=False)
-
-#     def accepted_days(self, yearoffset=0):User.objects.all()
-#         yearstart, yearend = get_year_start_end(yearoffset)
-#         qs = Holiday.objects.filter(holiday_request__employee__exact=self, holiday_request__status__exact=1, date__gte=yearstart, date__lt=yearend).distinct()
-#         return qs
-        
-#     def allowance_days(self, yearoffset=0):
-#         yearstart, yearend = get_year_start_end(yearoffset)
-#         if  self.start_date > yearstart or (self.end_date and self.end_date < yearend):
-#             if self.end_date:
-#                 if self.end_date < yearstart:
-#                     return 0 #always zero
-#                 elif self.end_date < yearend and self.start_date > yearstart:
-#                     time_diff = self.end_date - self.start_date
-#                 elif self.end_date < yearend:
-#                     time_diff = self.end_date - yearstart
-#             else: # self.start_date > yearstart:
-#                 time_diff = yearend - self.start_date
-#             # Added 1 day to the time difference, as you work both your first and last day.
-#             # (eg the datedifference of today - yesterday is 1, but you've worked two days)
-#             return ((time_diff.days +1) / 365.25) * self.holiday_per_annum
-#         else:
-#             return self.holiday_per_annum
-
-#     def days_left_count(self, yearoffset=0):
-#         alldays = self.accepted_days(yearoffset)
-#         days_left = self.allowance_days(yearoffset) - (alldays.count() - (alldays.filter(half_day__exact=True).count() * 0.5))
-#         return round(days_left, 2)
-
-#     def pending_requests(self):
-#         return HolidayRequest.objects.filter(status__exact=0, employee__exact=self)
-
-#     def managed_pending_requests(self):
-#         return HolidayRequest.objects.filter(employee__manager__exact=self, status__exact=0)
 
 
 class HolidayRequestManager(models.Manager):
@@ -83,16 +39,16 @@ class HolidayRequest(models.Model):
     ACCEPTED_STATUS = 1
     DENIED_STATUS = 2
     STATUS_CHOICES = (
-        (PENDING_STATUS,'Pending'),
-        (ACCEPTED_STATUS,'Accepted'),
-        (DENIED_STATUS,'Denied')
+        (PENDING_STATUS, 'Pending'),
+        (ACCEPTED_STATUS, 'Accepted'),
+        (DENIED_STATUS, 'Denied')
     )
     employee = models.ForeignKey(Profile)
     status = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=0)
     date_made = models.DateTimeField(_('request date'), auto_now_add=True)
     administrator = models.ForeignKey(Profile, blank=True, null=True, related_name='admin_set')
     employee_comment = models.TextField(_('Comments'), blank=True, null=True)
-    manager_comment  = models.TextField(_('Comments'), blank=True, null=True)
+    manager_comment = models.TextField(_('Comments'), blank=True, null=True)
 
     objects = HolidayRequestManager()
 
@@ -101,9 +57,9 @@ class HolidayRequest(models.Model):
 
     def start_day(self):
         try:
-             return self.holiday_set.all()[0]
+            return self.holiday_set.all()[0]
         except IndexError:
-             return None
+            return None
 
     def requested_days(self):
         return Holiday.objects.filter(holiday_request__employee__exact=self.employee, holiday_request__exact=self)
@@ -124,7 +80,7 @@ class HolidayRequest(models.Model):
         site = Site.objects.get_current()
         mailextras = {
                      'hol_req': self,
-                     'site':site,
+                     'site': site,
                      }
         render_to_send_mail(recipient_list=(self.employee.manager.email,), template='holiday/request_email.txt', extra_context=mailextras, subject='Holiday Request')
 
@@ -132,7 +88,7 @@ class HolidayRequest(models.Model):
         site = Site.objects.get_current()
         mailextras = {
                      'hol_req': self,
-                     'site':site,
+                     'site': site,
                      }
         render_to_send_mail(recipient_list=(self.employee.email,), template='holiday/confirmation_email.txt', extra_context=mailextras, subject='Re: Holiday Request')
 
@@ -155,13 +111,13 @@ class Holiday(models.Model):
     half_day = models.BooleanField(_('half day'), default=False)
 
     class Meta():
-        ordering = ['date',]
+        ordering = ['date']
 
     def __unicode__(self):
         if self.half_day:
-            return u"%s (half day)" % (format(self.date,settings.DATE_FORMAT))
+            return u"%s (half day)" % (format(self.date, settings.DATE_FORMAT))
         else:
-            return u"%s" % (format(self.date,settings.DATE_FORMAT))
+            return u"%s" % (format(self.date, settings.DATE_FORMAT))
 
 
 class BankHoliday(models.Model):
@@ -170,10 +126,10 @@ class BankHoliday(models.Model):
     name = models.CharField(_('name'), default='bank holiday', max_length=255)
 
     def __unicode__(self):
-        return u"%s (%s)" % (self.name, format(self.date,settings.DATE_FORMAT))
+        return u"%s (%s)" % (self.name, format(self.date, settings.DATE_FORMAT))
 
     class Meta():
-        ordering = ['-date',]
+        ordering = ['-date']
 
 #-------
 
