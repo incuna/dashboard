0b64e154f8aad76e5b3858345e2e4235d298322c
George Hickman <george@ghickman.co.uk>
2012-05-11 09:54:08 +0100

    Clean up the settings

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -1,11 +1,16 @@
 # Django settings for dashboard project.
 
-from os.path import abspath, dirname, join
+import os
 
-DIRNAME = abspath(dirname(__file__))
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+
+
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = os.environ.get('DEBUG', False)
 
 ADMINS = (
-    ('George Hickman', 'george@ghickman.co.uk'),
+    ('Admin', 'bugs@incuna.com'),
 )
 
 MANAGERS = ADMINS
@@ -32,81 +37,33 @@ DATABASES = {
 
 DATABASE_ROUTERS = ['status.modules.redmine.routers.RedmineRouter']
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'Europe/London'
+USE_TZ = True
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-GB'
+USE_I18N = False # Internationalization
+USE_L10N = True # Locale
 
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = join(DIRNAME, 'client_media')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_ROOT = os.path.join(DIRNAME, 'client_media')
 MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = join(DIRNAME, 'static_media')
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(DIRNAME, 'static_media')
 STATIC_URL = '/static/'
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
 ADMIN_MEDIA_PREFIX = '/static/admin/'
-
-# URL that handles the incuna specific media files
 INCUNA_MEDIA_URL = STATIC_URL + 'incuna/'
-
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    ('', join(DIRNAME, 'static')),
+    ('', os.path.join(DIRNAME, 'static')),
 )
-
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '_o273km705m5xdai56i5g%bd3jiaj60h$f87q+#e2phuyvi2c4'
-
-# List of callables that know how to import templates from various sources.
+TEMPLATE_DIRS = (os.path.join(DIRNAME, 'templates'),)
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
+TEMPLATE_CONTEXT_PROCESSORS += ('django.core.context_processors.request',)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -116,17 +73,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
-TEMPLATE_CONTEXT_PROCESSORS += ('django.core.context_processors.request',)
-
 ROOT_URLCONF = 'dashboard.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    join(DIRNAME, 'templates'),
-)
+SITE_ID = 1
+SECRET_KEY = '_o273km705m5xdai56i5g%bd3jiaj60h$f87q+#e2phuyvi2c4'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
