221423286b566c12ae82ad032002af876fbf2c6a
Charlie Denton <charlie@meshy.co.uk>
2012-05-11 14:16:51 +0100

    Add default to employee start_date. (+ PEP8)

--- dashboard/extensions/holidays.py
+++ dashboard/extensions/holidays.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -7,34 +9,43 @@ from holiday.models import Holiday, HolidayRequest, get_year_start_end
 
 def _accepted_days(self, yearoffset=0):
     yearstart, yearend = get_year_start_end(yearoffset)
-    qs = Holiday.objects.filter(holiday_request__employee__exact=self, holiday_request__status__exact=1, date__gte=yearstart, date__lt=yearend).distinct()
+    qs = Holiday.objects.filter(
+        holiday_request__employee__exact=self,
+        holiday_request__status__exact=1,
+        date__gte=yearstart,
+        date__lt=yearend
+    ).distinct()
     return qs
-    
+
+
 def _allowance_days(self, yearoffset=0):
     yearstart, yearend = get_year_start_end(yearoffset)
     if  self.start_date > yearstart or (self.end_date and self.end_date < yearend):
         if self.end_date:
             if self.end_date < yearstart:
-                return 0 #always zero
+                return 0  # always zero
             elif self.end_date < yearend and self.start_date > yearstart:
                 time_diff = self.end_date - self.start_date
             elif self.end_date < yearend:
                 time_diff = self.end_date - yearstart
-        else: # self.start_date > yearstart:
+        else:  # self.start_date > yearstart:
             time_diff = yearend - self.start_date
         # Added 1 day to the time difference, as you work both your first and last day.
         # (eg the datedifference of today - yesterday is 1, but you've worked two days)
-        return ((time_diff.days +1) / 365.25) * self.holiday_per_annum
+        return ((time_diff.days + 1) / 365.25) * self.holiday_per_annum
     else:
         return self.holiday_per_annum
+
 
 def _days_left_count(self, yearoffset=0):
     alldays = self.accepted_days(yearoffset)
     days_left = self.allowance_days(yearoffset) - (alldays.count() - (alldays.filter(half_day__exact=True).count() * 0.5))
     return round(days_left, 2)
 
+
 def _pending_requests(self):
     return HolidayRequest.objects.filter(status__exact=0, employee__exact=self)
+
 
 def _managed_pending_requests(self):
     return HolidayRequest.objects.filter(employee__manager__exact=self, status__exact=0)
@@ -42,7 +53,7 @@ def _managed_pending_requests(self):
 
 def register(cls, admin_cls):
     cls.add_to_class('holiday_per_annum', models.IntegerField(_('annual holiday'), default=settings.HOLIDAY_DEFAULT_ANNUAL_DAYS))
-    cls.add_to_class('start_date', models.DateField(_('start date')))
+    cls.add_to_class('start_date', models.DateField(_('start date'), default=datetime.datetime.now))
     cls.add_to_class('end_date', models.DateField(_('end date'), blank=True, null=True))
     cls.add_to_class('manager', models.ForeignKey('self', null=True, blank=False, related_name='managed_employees'))
     cls.add_to_class('accepted_days', _accepted_days)
@@ -51,10 +62,8 @@ def register(cls, admin_cls):
     cls.add_to_class('pending_requests', _pending_requests)
     cls.add_to_class('managed_pending_requests', _managed_pending_requests)
 
-
     if admin_cls:
         if admin_cls.fieldsets:
             admin_cls.fieldsets.append(
-                (_('Holidays'), { 'fields': ('holiday_per_annum', 'start_date', 'end_date'), 'classes': ('collapse',)})
+                (_('Holidays'), {'fields': ('holiday_per_annum', 'start_date', 'end_date'), 'classes': ('collapse',)})
             )
-
