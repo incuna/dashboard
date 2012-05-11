c3c03e77d3a2904ff53c8552dddaca606338403a
Charlie Denton <charlie@meshy.co.uk>
2012-05-11 14:42:58 +0100

    Add holiday fields migration

---
+++ dashboard/project_migrations/profiles/0004_add_holiday_fields.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Profile.holiday_per_annum'
+        db.add_column('profiles_profile', 'holiday_per_annum',
+                      self.gf('django.db.models.fields.IntegerField')(default=20),
+                      keep_default=False)
+
+        # Adding field 'Profile.start_date'
+        db.add_column('profiles_profile', 'start_date',
+                      self.gf('django.db.models.fields.DateField')(default=datetime.datetime.now),
+                      keep_default=False)
+
+        # Adding field 'Profile.end_date'
+        db.add_column('profiles_profile', 'end_date',
+                      self.gf('django.db.models.fields.DateField')(null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Profile.manager'
+        db.add_column('profiles_profile', 'manager',
+                      self.gf('django.db.models.fields.related.ForeignKey')(related_name='managed_employees', null=True, to=orm['profiles.Profile']),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Profile.holiday_per_annum'
+        db.delete_column('profiles_profile', 'holiday_per_annum')
+
+        # Deleting field 'Profile.start_date'
+        db.delete_column('profiles_profile', 'start_date')
+
+        # Deleting field 'Profile.end_date'
+        db.delete_column('profiles_profile', 'end_date')
+
+        # Deleting field 'Profile.manager'
+        db.delete_column('profiles_profile', 'manager_id')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'profiles.profile': {
+            'Meta': {'object_name': 'Profile', '_ormbases': ['auth.User']},
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'holiday_per_annum': ('django.db.models.fields.IntegerField', [], {'default': '20'}),
+            'is_manager': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'manager': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'managed_employees'", 'null': 'True', 'to': "orm['profiles.Profile']"}),
+            'ssh_key': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime.now'}),
+            'user_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True', 'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['profiles']
