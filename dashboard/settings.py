b40731d393991e29ac9d35771bca4c5202900d1c
George Hickman <george@ghickman.co.uk>
2012-07-13 09:40:01 +0100

    tidy up requirements and settings

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -1,29 +1,27 @@
 # Django settings for dashboard project.
-
 import os
 
-import dj_database_url
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+import django_cache_url
+import dj_database_url
 
 
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = bool(os.environ.get('DEBUG', False))
 
-ADMINS = (
-    ('Admin', 'bugs@incuna.com'),
-)
-
+ADMINS = (('Admin', 'bugs@incuna.com'),)
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config()}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost/dashboard')}
+CACHES = {'default': django_cache_url.config()}
 
-TIME_ZONE = 'Europe/London'
+TIME_ZONE = 'UTC'
+USE_L10N = True # Locale
 USE_TZ = True
 
 LANGUAGE_CODE = 'en-GB'
 USE_I18N = False # Internationalization
-USE_L10N = True # Locale
 
 # S3 Backend
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -35,7 +33,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 MEDIA_ROOT = os.path.join(DIRNAME, 'client_media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(DIRNAME, 'static_media')
-STATIC_URL = 'http://{0}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
+STATIC_URL = '/static/' if DEBUG else 'http://{0}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
 INCUNA_MEDIA_URL = STATIC_URL + 'incuna/'
 STATICFILES_DIRS = (('', os.path.join(DIRNAME, 'static')),)
 STATICFILES_FINDERS = (
@@ -44,6 +42,7 @@ STATICFILES_FINDERS = (
 )
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
+TEMPLATE_DEBUG = DEBUG
 TEMPLATE_DIRS = (os.path.join(DIRNAME, 'templates'),)
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -78,6 +77,7 @@ SOCIAL_AUTH_USER_MODEL = 'profiles.Profile'
 ROOT_URLCONF = 'dashboard.urls'
 SITE_ID = 1
 SECRET_KEY = '_o273km705m5xdai56i5g%bd3jiaj60h$f87q+#e2phuyvi2c4'
+WSGI_APPLICATION = 'dashboard.wsgi.application'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -110,26 +110,20 @@ INSTALLED_APPS = (
     'status.modules.weather',
 )
 
-SOUTH_MIGRATION_MODULES = {
-    'profiles': 'dashboard.project_migrations.profiles',
-}
-
-BBC_HOURLY_WEATHER = 'http://newsrss.bbc.co.uk/weather/forecast/25/ObservationsRSS.xml'
-
-BBC_WEEKLY_WEATHER = 'http://newsrss.bbc.co.uk/weather/forecast/25/Next3DaysRSS.xml'
-
-#HOLIDAYCAL_URL = 'http://holiday.incuna.com'
-
-HOLIDAY_DEFAULT_ANNUAL_DAYS = 20
-
-IMC_DEFAULT_PERIOD = 14
-
+SENTRY_DSN = 'http://e14704ffb3bd45a49c2411066163acd2:daf162a3a494436e9acc82ba8788e8c6@sentry.incuna.com/14'
+SENTRY_TESTING = DEBUG
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
@@ -142,7 +136,25 @@ LOGGING = {
     }
 }
 
-try:
-    from local_settings import *
-except ImportError, e:
-    pass
+# Debug Toolbar
+DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS': False}
+INTERNAL_IPS = ('127.0.0.1',)
+
+# Holiday
+HOLIDAY_DEFAULT_ANNUAL_DAYS = 20
+
+# imc
+IMC_DEFAULT_PERIOD = 14
+
+# Redmine
+#HOLIDAYCAL_URL = 'http://holiday.incuna.com'
+
+# South
+SOUTH_MIGRATION_MODULES = {
+    'profiles': 'dashboard.project_migrations.profiles',
+}
+
+# Weather
+BBC_HOURLY_WEATHER = 'http://newsrss.bbc.co.uk/weather/forecast/25/ObservationsRSS.xml'
+BBC_WEEKLY_WEATHER = 'http://newsrss.bbc.co.uk/weather/forecast/25/Next3DaysRSS.xml'
+
